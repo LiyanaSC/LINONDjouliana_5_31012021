8523940734d26ffdd88eaf1065f2f8801a847f53
OPBS Liyana <liyana.perso@gmail.com>
2021-02-16 17:50:23 -0400

    mise enplace des boites derésultats

--- javascript/dist/show_result.dev.js
+++ javascript/dist/show_result.dev.js
@@ -1,28 +1,90 @@
 "use strict";
 
-var teddy = ["teddy_1", "teddy_2", "teddy_3", "teddy_4", "teddy_5"];
-var _iteratorNormalCompletion = true;
-var _didIteratorError = false;
-var _iteratorError = undefined;
+// REQUETTE AJAX ----------------------------------------------------------------------------------------------------
+//variables pour afficher qu'il s'agit d'une erreur ajax
+var catchError = function catchError(e) {
+  console.error('ajax error', e);
+}; //fonction Ajax pour ne pas répéter la requête
 
-try {
-  for (var _iterator = teddys[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-    var _teddy = _step.value;
-    var newElt = document.createElement("div");
-    var elt = document.getElementById("genericResult");
-    elt.appendChild(newElt);
-  }
-} catch (err) {
-  _didIteratorError = true;
-  _iteratorError = err;
-} finally {
-  try {
-    if (!_iteratorNormalCompletion && _iterator["return"] != null) {
-      _iterator["return"]();
-    }
-  } finally {
-    if (_didIteratorError) {
-      throw _iteratorError;
-    }
-  }
-}
+
+var get = function get(url) {
+  return new Promise(function (resolve, reject) {
+    var request = new XMLHttpRequest();
+
+    request.onreadystatechange = function () {
+      if (this.readyState == 4) {
+        if (this.status === 200) {
+          resolve(request.responseText);
+        } else {
+          reject(request);
+        }
+      }
+    };
+
+    request.open("GET", url, true);
+    request.send();
+  });
+}; //promesse pour ne pas réitérer l'appelle de mon objet
+
+
+var getResults = function getResults() {
+  return new Promise(function (resolve, reject) {
+    get("http://localhost:3000/api/teddies").then(function (response) {
+      var teddys = JSON.parse(response);
+      resolve(teddys);
+    })["catch"](catchError);
+  });
+}; // appels de mes infos produits
+//tableau entier
+
+
+getResults().then(function (teddys) {
+  console.log(teddys);
+}); // résultat individuel
+
+getResults().then(function (teddys) {
+  var teddy1 = teddys[0];
+  console.log(teddy1);
+}); // RESULTAT DUPLIQUÉ ----------------------------------------------------------------------------------------------------
+//cration de l'élément originel
+
+var i;
+
+for (i = 1; i < 6; i++) {
+  //creation du conteneur
+  var result = document.createElement('div');
+  result.className = "main_products_results" + " " + "appear" + i;
+  result.id = "result_box" + i;
+  document.querySelector("section").appendChild(result); //creation de l'effet cliquable
+
+  var link = document.createElement('a');
+  link.className = "main_products_results_generic";
+  link.setAttribute('href', "#");
+  link.id = "link_" + i;
+  document.getElementById("result_box" + i).appendChild(link); //creation de la partie photo
+
+  var picBox = document.createElement('div');
+  picBox.className = "results_pics";
+  picBox.id = "result_pics" + i;
+  document.getElementById("link_" + i).appendChild(picBox); //url de la photo
+
+  var urlPic = getResults().then(function (teddys) {
+    var teddiesPic = teddys[0].imageUrl;
+    console.log("ici url image", teddiesPic);
+  });
+  var pic = document.createElement('img');
+  pic.className = "results_pics_size";
+  pic.id = "pic_" + i;
+  pic.setAttribute('src', "http://localhost:3000/images/teddy_" + [i] + ".jpg");
+  document.getElementById("result_pics" + i).appendChild(pic);
+} //4 clones
+
+/*let result = document.getElementById("result_box")
+var i;
+for (i = 0; i < 4; i++) {
+    var clone = result.cloneNode(true);
+    clone.id = 'teddies_box' + i;
+
+    document.querySelector("section").appendChild(clone);
+}*/
+//originel
--- javascript/show_result.js
+++ javascript/show_result.js
@@ -1,9 +1,115 @@
-let teddy = ["teddy_1", "teddy_2", "teddy_3", "teddy_4", "teddy_5"]
+// REQUETTE AJAX ----------------------------------------------------------------------------------------------------
 
-for (let teddy of teddys) {
-    const newElt = document.createElement("div");
-
-    let elt = document.getElementById("genericResult");
-
-    elt.appendChild(newElt)
+//variables pour afficher qu'il s'agit d'une erreur ajax
+let catchError = function(e) {
+    console.error('ajax error', e)
 }
+
+//fonction Ajax pour ne pas répéter la requête
+var get = function(url) {
+    return new Promise(function(resolve, reject) {
+        var request = new XMLHttpRequest()
+        request.onreadystatechange = function() {
+            if (this.readyState == 4) {
+                if (this.status === 200) {
+                    resolve(request.responseText)
+                } else {
+                    reject(request)
+                }
+
+            }
+        }
+        request.open("GET", url, true);
+        request.send()
+
+
+    })
+}
+
+//promesse pour ne pas réitérer l'appelle de mon objet
+var getResults = function() {
+    return new Promise(function(resolve, reject) {
+        get("http://localhost:3000/api/teddies").then(function(response) {
+            var teddys = JSON.parse(response)
+            resolve(teddys)
+        }).catch(catchError)
+
+    })
+}
+
+// appels de mes infos produits
+
+
+//tableau entier
+
+getResults().then(function(teddys) {
+    console.log(teddys)
+})
+
+// résultat individuel
+getResults().then(function(teddys) {
+    let teddy1 = teddys[0];
+    console.log(teddy1)
+})
+
+
+
+
+
+// RESULTAT DUPLIQUÉ ----------------------------------------------------------------------------------------------------
+//cration de l'élément originel
+var i;
+for (i = 1; i < 6; i++) {
+    //creation du conteneur
+    var result = document.createElement('div');
+    result.className = "main_products_results" + " " + "appear" + i;
+    result.id = "result_box" + i;
+    document.querySelector("section").appendChild(result);
+
+    //creation de l'effet cliquable
+    var link = document.createElement('a');
+    link.className = "main_products_results_generic";
+    link.setAttribute('href', "#");
+    link.id = "link_" + i;
+    document.getElementById("result_box" + i).appendChild(link);
+
+    //creation de la partie photo
+    var picBox = document.createElement('div');
+    picBox.className = "results_pics";
+    picBox.id = "result_pics" + i;
+    document.getElementById("link_" + i).appendChild(picBox);
+
+    //url de la photo
+    var urlPic = getResults().then(function(teddys) {
+        let teddiesPic = teddys[0].imageUrl;
+        console.log("ici url image", teddiesPic)
+    })
+    var pic = document.createElement('img');
+    pic.className = "results_pics_size";
+    pic.id = "pic_" + i;
+    pic.setAttribute('src', "http://localhost:3000/images/teddy_" + [i] + ".jpg");
+    document.getElementById("result_pics" + i).appendChild(pic);
+
+
+
+
+
+
+
+
+
+}
+
+
+
+//4 clones
+/*let result = document.getElementById("result_box")
+var i;
+for (i = 0; i < 4; i++) {
+    var clone = result.cloneNode(true);
+    clone.id = 'teddies_box' + i;
+
+    document.querySelector("section").appendChild(clone);
+}*/
+
+//originel

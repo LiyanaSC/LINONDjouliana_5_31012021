3ae183d1290989345489a35b8f7b153c84bc075f
OPBS Liyana <liyana.perso@gmail.com>
2021-02-22 14:37:31 -0400

    javascript accueil

--- javascript/dist/show_result.dev.js
+++ javascript/dist/show_result.dev.js
@@ -39,52 +39,71 @@ var getResults = function getResults() {
 
 
 getResults().then(function (teddys) {
-  console.log(teddys);
-}); // résultat individuel
+  teddys.forEach(function (teddy, index) {
+    console.log(teddy, index); //creation du conteneur--------------------------------------------------------------------------------------------
 
-getResults().then(function (teddys) {
-  var teddy1 = teddys[0];
-  console.log(teddy1);
-}); // RESULTAT DUPLIQUÉ ----------------------------------------------------------------------------------------------------
-//cration de l'élément originel
+    var result = document.createElement('div');
+    result.className = "main_products_results" + " " + "appear" + index;
+    result.id = "result_box" + index;
+    document.querySelector("section").appendChild(result); //creation de l'effet cliquable
 
-var i;
+    var link = document.createElement('a');
+    link.className = "main_products_results_generic";
+    link.setAttribute('href', "#");
+    link.id = teddy._id + index;
+    document.getElementById("result_box" + index).appendChild(link); //creation de la partie photo
 
-for (i = 1; i < 6; i++) {
-  //creation du conteneur
-  var result = document.createElement('div');
-  result.className = "main_products_results" + " " + "appear" + i;
-  result.id = "result_box" + i;
-  document.querySelector("section").appendChild(result); //creation de l'effet cliquable
+    var picBox = document.createElement('div');
+    picBox.className = "results_pics";
+    picBox.id = "result_pics" + index;
+    document.getElementById(teddy._id + index).appendChild(picBox);
+    var pic = document.createElement('img');
+    pic.className = "results_pics_size";
+    pic.id = "pic_" + index;
+    pic.setAttribute('src', teddy.imageUrl);
+    document.getElementById("result_pics" + index).appendChild(pic); //creation de la partie commentaire
 
-  var link = document.createElement('a');
-  link.className = "main_products_results_generic";
-  link.setAttribute('href', "#");
-  link.id = "link_" + i;
-  document.getElementById("result_box" + i).appendChild(link); //creation de la partie photo
+    var commentBox = document.createElement('div');
+    commentBox.className = "results_descriptions";
+    commentBox.id = "results_descriptions" + index;
+    document.getElementById(teddy._id + index).appendChild(commentBox); //Partie h3 (nom des articles)
 
-  var picBox = document.createElement('div');
-  picBox.className = "results_pics";
-  picBox.id = "result_pics" + i;
-  document.getElementById("link_" + i).appendChild(picBox); //url de la photo
+    var productTitle = document.createElement('h3');
+    productTitle.className = "results_descriptions_title";
+    productTitle.id = "results_descriptions_title" + index;
+    document.getElementById("results_descriptions" + index).appendChild(productTitle);
+    var nameProduct = document.getElementById("results_descriptions_title" + index);
+    nameProduct.textContent = teddy.name; //Partie description
 
-  var urlPic = getResults().then(function (teddys) {
-    var teddiesPic = teddys[0].imageUrl;
-    console.log("ici url image", teddiesPic);
+    var productText = document.createElement('p');
+    productText.className = "results_descriptions_text";
+    productText.id = "product_text" + index;
+    document.getElementById("results_descriptions" + index).appendChild(productText);
+    var textDescription = document.getElementById("product_text" + index);
+    textDescription.textContent = teddy.description; //Prix
+
+    var productPrice = document.createElement('p');
+    productPrice.className = "results_descriptions_text_price";
+    productPrice.id = "product_price" + index;
+    document.getElementById("results_descriptions" + index).appendChild(productPrice);
+    var textPrice = document.getElementById("product_price" + index);
+    textPrice.textContent = (teddy.price / 100).toPrecision(4) + "€"; //creation de l'étiquette disponible
+
+    var availableLabel = document.createElement('div');
+    availableLabel.className = "results_availability";
+    availableLabel.id = "results_availability" + index;
+    document.getElementById(teddy._id + index).appendChild(availableLabel); //creation de l'étiquette disponible
+
+    var availableText = document.createElement('div');
+    availableText.id = "available" + index;
+    document.getElementById("results_availability" + index).appendChild(availableText);
+    var textAvailable = document.getElementById("available" + index);
+    textAvailable.textContent = "disponible"; //redirection -------------------------------------------------------------------------------------------------------
+
+    var teddyLinkAction = document.getElementById(teddy._id + index);
+    teddyLinkAction.addEventListener('click', function (event) {
+      event.preventDefault();
+      location.href = "teddy_1.html";
+    });
   });
-  var pic = document.createElement('img');
-  pic.className = "results_pics_size";
-  pic.id = "pic_" + i;
-  pic.setAttribute('src', "http://localhost:3000/images/teddy_" + [i] + ".jpg");
-  document.getElementById("result_pics" + i).appendChild(pic);
-} //4 clones
-
-/*let result = document.getElementById("result_box")
-var i;
-for (i = 0; i < 4; i++) {
-    var clone = result.cloneNode(true);
-    clone.id = 'teddies_box' + i;
-
-    document.querySelector("section").appendChild(clone);
-}*/
-//originel
+});
--- javascript/show_result.js
+++ javascript/show_result.js
@@ -6,7 +6,7 @@ let catchError = function(e) {
 }
 
 //fonction Ajax pour ne pas répéter la requête
-var get = function(url) {
+let get = function(url) {
     return new Promise(function(resolve, reject) {
         var request = new XMLHttpRequest()
         request.onreadystatechange = function() {
@@ -27,10 +27,10 @@ var get = function(url) {
 }
 
 //promesse pour ne pas réitérer l'appelle de mon objet
-var getResults = function() {
+let getResults = function() {
     return new Promise(function(resolve, reject) {
         get("http://localhost:3000/api/teddies").then(function(response) {
-            var teddys = JSON.parse(response)
+            let teddys = JSON.parse(response)
             resolve(teddys)
         }).catch(catchError)
 
@@ -39,77 +39,99 @@ var getResults = function() {
 
 // appels de mes infos produits
 
-
-//tableau entier
-
 getResults().then(function(teddys) {
-    console.log(teddys)
+    teddys.forEach(function(teddy, index) {
+        console.log(teddy, index)
+
+        //creation du conteneur--------------------------------------------------------------------------------------------
+        let result = document.createElement('div');
+        result.className = "main_products_results" + " " + "appear" + index;
+        result.id = "result_box" + index;
+        document.querySelector("section").appendChild(result);
+
+        //creation de l'effet cliquable
+        let link = document.createElement('a');
+        link.className = "main_products_results_generic";
+        link.setAttribute('href', "#");
+        link.id = teddy._id + index;
+        document.getElementById("result_box" + index).appendChild(link);
+
+        //creation de la partie photo
+        let picBox = document.createElement('div');
+        picBox.className = "results_pics";
+        picBox.id = "result_pics" + index;
+        document.getElementById(teddy._id + index).appendChild(picBox);
+
+        let pic = document.createElement('img')
+        pic.className = "results_pics_size";
+        pic.id = "pic_" + index;
+        pic.setAttribute('src', teddy.imageUrl);
+        document.getElementById("result_pics" + index).appendChild(pic);
+
+        //creation de la partie commentaire
+        let commentBox = document.createElement('div');
+        commentBox.className = "results_descriptions";
+        commentBox.id = "results_descriptions" + index;
+        document.getElementById(teddy._id + index).appendChild(commentBox);
+
+        //Partie h3 (nom des articles)
+
+        let productTitle = document.createElement('h3');
+        productTitle.className = "results_descriptions_title";
+        productTitle.id = "results_descriptions_title" + index;
+        document.getElementById("results_descriptions" + index).appendChild(productTitle);
+
+        let nameProduct = document.getElementById("results_descriptions_title" + index);
+        nameProduct.textContent = teddy.name;
+
+        //Partie description
+
+        let productText = document.createElement('p');
+        productText.className = "results_descriptions_text";
+        productText.id = "product_text" + index;
+        document.getElementById("results_descriptions" + index).appendChild(productText);
+
+        let textDescription = document.getElementById("product_text" + index);
+        textDescription.textContent = teddy.description;
+
+        //Prix
+
+
+        let productPrice = document.createElement('p');
+        productPrice.className = "results_descriptions_text_price";
+        productPrice.id = "product_price" + index;
+        document.getElementById("results_descriptions" + index).appendChild(productPrice);
+
+        let textPrice = document.getElementById("product_price" + index);
+        textPrice.textContent = (teddy.price / 100).toPrecision(4) + "€";
+
+        //creation de l'étiquette disponible
+        let availableLabel = document.createElement('div');
+        availableLabel.className = "results_availability";
+        availableLabel.id = "results_availability" + index;
+        document.getElementById(teddy._id + index).appendChild(availableLabel);
+
+        //creation de l'étiquette disponible
+        let availableText = document.createElement('div');
+        availableText.id = "available" + index;
+        document.getElementById("results_availability" + index).appendChild(availableText);
+
+        let textAvailable = document.getElementById("available" + index);
+        textAvailable.textContent = "disponible";
+
+        //redirection -------------------------------------------------------------------------------------------------------
+        const teddyLinkAction = document.getElementById(teddy._id + index);
+        teddyLinkAction.addEventListener('click', function(event) {
+            event.preventDefault();
+            location.href = "teddy_1.html";
+
+        })
+
+
+
+
+
+
+    });
+
 })
-
-// résultat individuel
-getResults().then(function(teddys) {
-    let teddy1 = teddys[0];
-    console.log(teddy1)
-})
-
-
-
-
-
-// RESULTAT DUPLIQUÉ ----------------------------------------------------------------------------------------------------
-//cration de l'élément originel
-var i;
-for (i = 1; i < 6; i++) {
-    //creation du conteneur
-    var result = document.createElement('div');
-    result.className = "main_products_results" + " " + "appear" + i;
-    result.id = "result_box" + i;
-    document.querySelector("section").appendChild(result);
-
-    //creation de l'effet cliquable
-    var link = document.createElement('a');
-    link.className = "main_products_results_generic";
-    link.setAttribute('href', "#");
-    link.id = "link_" + i;
-    document.getElementById("result_box" + i).appendChild(link);
-
-    //creation de la partie photo
-    var picBox = document.createElement('div');
-    picBox.className = "results_pics";
-    picBox.id = "result_pics" + i;
-    document.getElementById("link_" + i).appendChild(picBox);
-
-    //url de la photo
-    var urlPic = getResults().then(function(teddys) {
-        let teddiesPic = teddys[0].imageUrl;
-        console.log("ici url image", teddiesPic)
-    })
-    var pic = document.createElement('img');
-    pic.className = "results_pics_size";
-    pic.id = "pic_" + i;
-    pic.setAttribute('src', "http://localhost:3000/images/teddy_" + [i] + ".jpg");
-    document.getElementById("result_pics" + i).appendChild(pic);
-
-
-
-
-
-
-
-
-
-}
-
-
-
-//4 clones
-/*let result = document.getElementById("result_box")
-var i;
-for (i = 0; i < 4; i++) {
-    var clone = result.cloneNode(true);
-    clone.id = 'teddies_box' + i;
-
-    document.querySelector("section").appendChild(clone);
-}*/
-
-//originel

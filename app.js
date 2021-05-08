dab9a28f4e1c55e0bb119a54a33b268b2b0c7bd0
OPBS Liyana <liyana.perso@gmail.com>
2021-05-08 16:59:34 -0400

    Update app.js

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
     next();
 });
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.sendFile('index.html'))
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 

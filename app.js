5350be432ecdc73a3ea75b2462ccee4911aae60b
OPBS Liyana <liyana.perso@gmail.com>
2021-05-08 17:27:33 -0400

    Update app.js

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
 });
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/index.html')))
-    .use(path.join(__dirname, '/panier.html'))
+
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
